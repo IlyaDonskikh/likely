945e8223c0437e2d71323b8f0b2b11d162de24fd
doniv <donivrecord@gmail.com>
2015-11-15 21:56:19 +0300

    Add tests

--- test/dummy/app/assets/javascripts/application.js
+++ test/dummy/app/assets/javascripts/application.js
@@ -10,4 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require likely
 //= require_tree .
---
+++ test/dummy/app/controllers/presentation_controller.rb
@@ -0,0 +1,4 @@
+class PresentationController < ApplicationController
+  def index
+  end
+end
